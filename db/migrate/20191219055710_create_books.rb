84e244850afab55d5f1c3b00a2181d8cf4d2842b
1-sam <hayatonntonn_1212@yahoo.co.jp>
2019-12-19 15:31:20 +0900

    test

--- db/migrate/20191219055710_create_books.rb
+++ db/migrate/20191219055710_create_books.rb
@@ -1,9 +1,9 @@
 class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
-      t.String :title
-      t.String :author
-      t.date_time :published_on
+      t.string :title
+      t.string :author
+      t.datetime :published_on
       t.integer :price
       t.boolean :displayed
 
